b733a85cfc820e6d39bf6d39344108035caf45a3
s7eamy <52354209+s7eamy@users.noreply.github.com>
2024-09-03 09:53:50 +0300

    feat: add script for rendering recipes

---
+++ scripts/recipe_view_scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", () => {
+	const recipeId = window.location.pathname.split("/").pop();
+	fetch(`/recipes/${recipeId}`)
+		.then((response) => response.json())
+		.then((recipe) => {
+			document.getElementById("name").innerText = recipe.name;
+
+			document.getElementById("time").innerText = recipe.time;
+
+			const tagsEle = document.getElementById("tags");
+			recipe.tags.forEach((tag) => {
+				tagEle = document.createElement("span");
+				tagEle.innerText = tag;
+				tagsEle.appendChild(tagEle);
+			});
+
+			document.getElementById("description").innerText =
+				recipe.description;
+
+			const ingredientsEle = document.getElementById("ingredients");
+			recipe.ingredients.forEach((ingredient) => {
+				ingEle = document.createElement("li");
+				ingEle.innerText = ingredient;
+				ingredientsEle.appendChild(ingEle);
+			});
+
+			const instructionsEle = document.getElementById("instructions");
+			recipe.instructions.forEach((instruction) => {
+				insEle = document.createElement("li");
+				insEle.innerText = instruction;
+				instructionsEle.appendChild(insEle);
+			});
+		})
+		.catch((err) => console.error("Error fetching recipe:", err));
+});
